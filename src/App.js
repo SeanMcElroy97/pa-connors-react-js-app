ae431ac0f1e793c89eb819399df3412997c2dc6f
unknown <seanpetermcelroy@gmail.com>
2020-12-20 23:00:31 +0000

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import  Players  from "./components/players";
 import React  from 'react';
+import {Route, Switch, Redirect} from 'react-router-dom'
+import  Players  from "./components/players";
+import NavBar from './components/navbar';
 
 function App() {
   return (
     <React.Fragment>
-      <Players/>
+      <NavBar/>
+      <Switch>
+        <Route path="/players" component={Players}/>
+        <Redirect to="/players"/>
+      </Switch>
+      
     </React.Fragment>
   );
 }
