ba57d1c80ff63d73cdf414e0064f29b4b7ee6d1f
unknown <seanpetermcelroy@gmail.com>
2020-12-20 23:07:17 +0000

    Added events component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React  from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom'
-import  Players  from "./components/players";
 import NavBar from './components/navbar';
+import  Players  from "./components/players";
+import Events from './components/events';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar/>
       <Switch>
         <Route path="/players" component={Players}/>
+        <Route path="/events" component={Events}/>
         <Redirect to="/players"/>
       </Switch>
       
---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Players from './players';
+
+const NavBar = () => {
+    return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+    <a className="navbar-brand" href="#">Pa Connors GFC</a>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+      <NavLink className="nav-item nav-link" to="/players">Players</NavLink>
+      <NavLink className="nav-item nav-link" to="/events">Events</NavLink>
+        
+      </div>
+    </div>
+  </nav>);
+}
+ 
+export default NavBar;
