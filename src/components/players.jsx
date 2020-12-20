866fd4c259f39be14e9cdc2156a24a80680f8401
unknown <seanpetermcelroy@gmail.com>
2020-12-20 22:28:43 +0000

    updated players component added bold text

--- src/components/players.jsx
+++ src/components/players.jsx
@@ -10,17 +10,20 @@ class Players extends Component {
         const {players} = this.state;
         return ( 
         <div className="container">
-            {players.map(p=>
-            <div className="card bg-warning" style={{width: '18rem'}}>
-                <div class="card-header text-center"><h5 className="card-title">{p.fullName} {getPlayerOverallRating(p.id)}</h5></div>
-                <img className="card-img-top" src="../unlockableCharacter.jpg" style={{height: '200px'}} alt={`${p.fullName} img`}></img>
-                <div className="card-body">
-                    <p className="card-text">Drinking : {p.drinkingAbility}</p>
-                    <p className="card-text">Poker : {p.pokerAbility}</p>
-                    <p className="card-text">Golf : {p.golfingAbility}</p>
+            <div class="card-columns">
+                {players.map(p=>
+                <div className="card bg-warning" style={{width: '18rem'}}>
+                    <div class="card-header text-center"><h2 className="card-title">{getPlayerOverallRating(p.id)}</h2></div>
+                    <div class="card-header text-center"><h5 className="card-title">{p.fullName} </h5></div>
+                    <img className="card-img-top" src="../unlockableCharacter.jpg" style={{height: '200px'}} alt={`${p.fullName} img`}></img>
+                    <div className="card-body">
+                        <p className="card-text">Drinking ability: <b>{p.drinkingAbility}</b></p>
+                        <p className="card-text">Poker ability: <b>{p.pokerAbility}</b></p>
+                        <p className="card-text">Golf ability: <b>{p.golfingAbility}</b></p>
+                    </div>
                 </div>
+                )}
             </div>
-            )}
         </div> );
     }
 }
